c6a2e44a989312f9e08b252b2618443b215dfa87
Andre Mendes <andre.eletrom@hotmail.com>
2023-04-04 16:05:20 -0300

    feat: data migrations and context

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            //Database.EnsureCreated();
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
