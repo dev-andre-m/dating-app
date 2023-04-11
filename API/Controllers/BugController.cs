2f7698138f6aacdd1236049ae3482ea01d7e32a9
Andre Mendes <andre.eletrom@hotmail.com>
2023-04-11 16:06:32 -0300

    feat: added exception middleware and bug controller

---
+++ API/Controllers/BugController.cs
@@ -0,0 +1,48 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BugController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public BugController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var errorObject = _context.Users.Find(-1);
+            if (errorObject == null) return NotFound();
+            return errorObject;
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var errorObject = _context.Users.Find(-1);
+            var errorObjectToReturn = errorObject.ToString();
+            return errorObjectToReturn;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("Bad request");
+        }
+
+
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using API.Data;
 using API.Extensions;
 using API.Interface;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -17,10 +18,12 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200/"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
