9448a1c77161d72ef8b95ac90ff7084a7391ce3f
Andre Mendes <andre.eletrom@hotmail.com>
2023-04-17 10:13:14 -0300

    fix: add-photo endpoint now returns correct status:201 Created

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -64,9 +64,11 @@ namespace API.Controllers
 
             if(user.Photos.Count == 0) photo.IsMain = true;
             user.Photos.Add(photo);
-            if(await _userRepository.SaveAllAsync()) return _mapper.Map<PhotoDto>(photo);
+            if (await _userRepository.SaveAllAsync())
+            { 
+                return CreatedAtAction(nameof(GetUser), new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+            }
             return BadRequest();
-
         }
 
     }
