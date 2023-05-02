1549ff0aeee8ef50fc10268c366e3a934bedae98
Andre Mendes <andre.eletrom@hotmail.com>
2023-05-02 16:50:26 -0300

    feat: added controller fallback, changed db connection from SQLite to PostgreSQL, removed old Data migrations

--- API/Program.cs
+++ API/Program.cs
@@ -29,6 +29,7 @@ app.UseStaticFiles();
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
@@ -39,7 +40,7 @@ try
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
-    await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]");
+    await Seed.ClearConnections(context);
     await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception ex)
